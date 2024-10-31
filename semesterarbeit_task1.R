8f1628bb3bc8204c521b97d2097befd5fbc7e288
Chr1s28 <cbosshard28@gmail.com>
2024-10-31 13:59:53 +0100

    - task 2!

--- semesterarbeit_task1.R
+++ semesterarbeit_task1.R
@@ -1,8 +1,9 @@
 kickstarter <- read.csv("./data/kickstarter_projects.csv", encoding = "UTF-8")
 ## Grafik 1 ----
 category.relative.frequency <- table(kickstarter$Category) / length(kickstarter$Category) * 100
+
 par(mar=c(8, 6, 4, 2))
-balken <- barplot(
+category.barplot <- barplot(
   sort(category.relative.frequency, decreasing = TRUE),
   las = 2,
   main = "Amount of Kickstarter projects by category",
@@ -13,7 +14,7 @@ balken <- barplot(
 title(xlab="Category", line=6)
 title(ylab="Relative frequency (%)", line=3)
 text(
-  x = balken,
+  x = category.barplot,
   y = sort(category.relative.frequency, decreasing = TRUE) + 0.5,
   labels = round(
     sort(category.relative.frequency,
@@ -21,11 +22,17 @@ text(
     ),
     1)
 )
+
+# Mean relative frequency abline
+abline(h = mean(category.relative.frequency), col = "red", lty = 2, lwd = 2)
+text(x = max(category.barplot), y = mean(category.relative.frequency), labels = paste("Mean:", round(mean(category.relative.frequency), 2)), pos = 3, col = "red")
+
 ## Grafik 2 ----
 library(plotly)
 library(countrycode)
 
 country.freq <- table(kickstarter$Country)
+country.rel.freq <- round(country.freq / length(kickstarter$Country) * 100, digits = 2)
 
 # dark grey boundaries
 l <- list(color = toRGB("darkgray"), width = 0.5)
@@ -48,10 +55,15 @@ fig <- plot_geo(kickstarter)
 fig <- fig %>% add_trace(
   z = log10(country.freq),
   color = log10(country.freq),
-  text = paste("Country: ", names(country.freq),
-               "<br>Projects: ", country.freq),
+  text = paste("<b>Country:</b>", names(country.freq),
+               "<br><b>Projects:</b>", format(country.freq, big.mark="'"),
+               "<br><b>Relative:</b>", paste0(country.rel.freq, "%")
+  ),
   locations = countrycode(names(country.freq), "country.name", "iso3c"),
-  marker = list(line = l, type = "log")
+  marker = list(line = l, type = "log"),
+  hovertemplate = paste(
+    "%{text}<extra></extra>"
+  )
 )
 
 country.log_max_min <- seq(floor(log10(min(country.freq))), ceiling(log10(max(country.freq))), by = 1)
